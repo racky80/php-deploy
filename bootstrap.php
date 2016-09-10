48100cedbe7b817be0a947c2774202e9e40a817b
Rolf den Hartog <rolf@rolfdenhartog.nl>
2016-09-10 22:40:15 +0200

    - Created a separate file for the constants - Deleted the composer.lock file

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author Rolf den Hartog <rolf@rolfdenhartog.nl>
+ */
+
+/**
+ * Include the composer autoloader.
+ */
+require_once __DIR__ . '/../../autoload.php';
+
+/**
+ * Defines the paths to this package and to the project.
+ */
+define('PHP_DEPLOY_DIR', __DIR__);
+define('PHP_DEPLOY_PROJECT_DIR', PHP_DEPLOY_DIR . '/../../..');
