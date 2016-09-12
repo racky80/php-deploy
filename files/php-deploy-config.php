7058c69db05575e3169070f7e8b24e2a016f1858
Rolf den Hartog <rolf@rolfdenhartog.nl>
2016-09-12 17:51:19 +0200

    Created the ReleaseCreateCommand

--- files/php-deploy-config.php
+++ files/php-deploy-config.php
@@ -22,5 +22,58 @@ return [
             'feature/your-very-cool-feature'    => '/var/www/domains/feature.example.org',
             'hotfix/fixing-something-important' => '/var/www/domains/hotfix.example.org',
         ],
+
+        /**
+         * This key contains the information to create new releases to a production environment.
+         */
+        'deployment' => [
+
+            /**
+             * The source directory to copy the file from.
+             */
+            'from'     => '/var/www/domains/accept.example.org',
+
+            /**
+             * The destination of the files to be copied to.
+             */
+            'to'       => '/var/www/domains/example.org',
+
+            /**
+             * You may want to ignore files to be copied. The paths are relative to the directory in 'from'.
+             */
+            'ignore'   => [
+
+                /**
+                 * Ignore a complete directory. `./vendor` will not be copied including the files in this directory. If
+                 * there is a subdirectory, for example `./path/to/another/vendor`, it will not be ignored.
+                 */
+                '/vendor',
+
+                /**
+                 * Ignore a single file. `./composer.json` will be ignored, but `./subdir/composer.json` will be copied.
+                 */
+                '/composer.json',
+
+                /**
+                 * Ignore a directory anywhere in the project. If there are multiple directories with this name, they
+                 * will all be ignored. `./node_modules` will be ignored and `./path/to/another/node_modules` too.
+                 */
+                'node_modules',
+
+                /**
+                 * Ignore a file anywhere in the project. `./resources/assets/bower.json` will not be copied. If there
+                 * are multiple files with the same name, they will al be ignored.
+                 */
+                'bower.json',
+            ],
+
+            /**
+             * Create symlinks. Paths are relative from the directory in 'to'.
+             */
+            'symlinks' => [
+                '/shared/uploads' => '/current/public/uploads',
+                '/shared/.env'    => '/current/.env',
+            ],
+        ],
     ],
 ];
---
+++ src/Commands/ReleaseCreateCommand.php
@@ -0,0 +1,238 @@
+<?php
+
+/**
+ * @author Rolf den Hartog <rolf@rolfdenhartog.nl>
+ */
+
+namespace PhpDeploy\Commands;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * ReleaseCreateCommand class.
+ */
+class ReleaseCreateCommand extends AbstractCommand
+{
+    /**
+     * @var string
+     */
+    private $repository;
+
+    /**
+     * @var bool
+     */
+    private $isDryRun = false;
+
+    /**
+     * @var Filesystem
+     */
+    private $fileSystem;
+
+    /**
+     * @var string
+     */
+    private $from;
+
+    /**
+     * @var string
+     */
+    private $to;
+
+    /**
+     * @var array
+     */
+    private $ignore = [];
+
+    /**
+     * @var array
+     */
+    private $symlinks = [];
+
+    /**
+     * @var string
+     */
+    private $releaseDirectory;
+
+    /**
+     * Configures the command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('release:create')
+            ->setDescription('Deploys a website or application by making a new release directory and copying the files')
+            ->addArgument('repository', InputArgument::REQUIRED, 'The repository of the website or application you want to to be released')
+            ->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Test the deployment by only showing the output');
+    }
+
+    /**
+     * Executes the command.
+     *
+     * @throws \Exception
+     * @return integer
+     */
+    protected function executeCommand()
+    {
+        $this->repository = $this->input->getArgument('repository');
+        $this->isDryRun   = $this->input->getOption('dry-run');
+        $configuration    = array_get(\PhpDeploy\config($this->repository), 'deployment', []);
+
+        if (empty($configuration)) {
+            throw new \Exception(sprintf('No configuration found to deploy \'%s\'', $this->repository));
+        }
+
+        $this->fileSystem = new Filesystem();
+        $this->from       = array_get($configuration, 'from');
+        $this->to         = array_get($configuration, 'to');
+        $this->ignore     = array_merge(['/.git'], array_get($configuration, 'ignore'));
+        $this->symlinks   = array_get($configuration, 'symlinks');
+
+//        $this->createDirectory();
+//        $this->copyFiles();
+//        $this->createSymlinks();
+//        $this->changeCurrent();
+        $this->deleteOldReleases();
+
+        return 0;
+    }
+
+    /**
+     * Creates the directory.
+     */
+    private function createDirectory()
+    {
+        $this->releaseDirectory = $this->to . '/releases/' . date('YmdHis');
+
+        $this->writeln('Create directory for new release: ' . $this->releaseDirectory);
+
+        if ($this->isDryRun === false && mkdir($this->releaseDirectory, 0755, true) === false) {
+            throw new \Exception('Release directory not created');
+        }
+    }
+
+    /**
+     * - Gets all the directories and files from the source directory.
+     * - Maps the SplFileInfo object to a simple string and removes the part which is the same as the source directory.
+     * - Filters out the directories and files that needs to be ignored.
+     * - Loops the files and calls the copyFile method.
+     */
+    private function copyFiles()
+    {
+        $this->writeln('<comment>Creating file list. This could take a while...</comment>');
+
+        $recursiveDirectoryIterator = new \RecursiveDirectoryIterator($this->from, \FilesystemIterator::SKIP_DOTS);
+        $recursiveIteratorIterator  = new \RecursiveIteratorIterator($recursiveDirectoryIterator, \RecursiveIteratorIterator::SELF_FIRST);
+
+        $files = collect($recursiveIteratorIterator)
+            ->map(function (\SplFileInfo $splFileInfo) {
+                return str_replace($this->from, '', $splFileInfo->getPathname());
+            })
+            ->filter(function (string $path) {
+                foreach ($this->ignore as $pattern) {
+                    // path and pattern starting with a slash
+                    if (substr($pattern, 0, 1) === '/' && strpos($path, $pattern) === 0) {
+                        $this->writeln(sprintf('Ignoring file \'%s\'', $path));
+
+                        return false;
+                    }
+
+                    // path and pattern NOT starting with a slash
+                    if (substr($pattern, 0, 1) !== '/' && strpos($path, $pattern) !== false) {
+                        $this->writeln(sprintf('Ignoring file \'%s\'', $path));
+
+                        return false;
+                    }
+                }
+
+                return true;
+            });
+
+        foreach ($files as $file) {
+            $this->copyFile($file);
+        }
+    }
+
+    /**
+     * Creates the directory or copies the file.
+     *
+     * @param string $path
+     */
+    private function copyFile(string $path)
+    {
+        $this->writeln(sprintf('Copying file \'%s\'', $path));
+
+        if ($this->isDryRun) {
+            return;
+        }
+
+        $from = $this->from . $path;
+        $to   = $this->releaseDirectory . $path;
+
+        if (is_dir($from) && mkdir($to) === false) {
+            $this->writeln(sprintf('<error>Directory \'%s\' not created</error>', $path));
+        } elseif (is_file($from) && copy($from, $to) === false) {
+            $this->writeln(sprintf('<error>File \'%s\' not copied</error>', $path));
+        }
+    }
+
+    /**
+     * Creates the symlinks.
+     */
+    private function createSymlinks()
+    {
+        foreach ($this->symlinks as $original => $symlink) {
+            $this->writeln('Creating symlink: ' . $original . ' => ' . $symlink);
+
+            $from = $this->to . $original;
+            $to   = $this->releaseDirectory . $symlink;
+
+            if ($this->isDryRun === false && symlink($from, $to) === false) {
+                $this->writeln(sprintf('<error>Symlink not created (%s => %s)</error>', $original, $symlink));
+            }
+        }
+    }
+
+    /**
+     * Changes the symlink for 'current'.
+     */
+    private function changeCurrent()
+    {
+        if ($this->isDryRun) {
+            return;
+        }
+
+        $from = $this->releaseDirectory;
+        $to   = $this->to . '/current';
+
+        unlink($to);
+
+        if (symlink($from, $to) === false) {
+            $this->writeln(sprintf('<error>Symlink not created (%s => %s)</error>', $from, $to));
+        }
+    }
+
+    /**
+     * Deletes the old releases.
+     */
+    private function deleteOldReleases()
+    {
+        $releases = glob($this->to . '/releases/*', GLOB_ONLYDIR);
+        $count    = count($releases);
+
+        if ($count <= 5) {
+            $this->writeln('<comment>No releases found to delete</comment>');
+        }
+
+        while (count($releases) > 5) {
+            $release = array_shift($releases);
+
+            $this->writeln(sprintf('Deleting release \'%s\'', preg_replace('/.*\/([0-9]+)/', '$1', $release)));
+
+            if ($this->isDryRun === false) {
+                $this->fileSystem->remove($release);
+            }
+        }
+    }
+}
