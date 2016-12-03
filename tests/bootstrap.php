549eb3a988cad05afaa68e410ffb870c1386d998
Rolf den Hartog <rolf@rolfdenhartog.nl>
2016-12-04 00:14:35 +0100

    Created tests for the PullCommand class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
  * Remove the files from the logs directory.
  */
 shell_exec('rm -rf ' . \PhpDeploy\path('/logs/tests') . '/*');
+
+/**
+ * Changes the working directory.
+ */
+chdir(\PhpDeploy\path('/logs/tests'));
